d45286d010095c450a58ecdde4f8d51fb797d3b0
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-17 00:14:25 +0200

    alx-fe-reactjs

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,11 @@
-function UserProfile (props) {
+const UserProfile (props) {
     return (
         <div>
         <h2>{props.name}</h2>
-        <p>{props.age}</p>
-        <p>{props.bio}</p>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
         </div>
     );
 };
+
+export default UserProfile;
