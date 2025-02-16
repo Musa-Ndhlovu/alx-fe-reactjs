5644191cc2223b08a7847c48c3d3d346756ce826
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-16 23:42:04 +0200

    alx-fe-reactjs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+
 
 
 function App() {
@@ -9,6 +14,8 @@ function App() {
 
   return (
     <>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +36,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
+
+
   )
 }
 
---
+++ alx-react-app/src/UserProfile.jsx
@@ -0,0 +1,9 @@
+function UserProfile (props) {
+    return (
+        <div>
+        <h2>{props.name}</h2>
+        <p>{props.age}</p>
+        <p>{props.bio}</p>
+        </div>
+    );
+};
