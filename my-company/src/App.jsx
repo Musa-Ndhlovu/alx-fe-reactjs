e7c8b374ebe584b680d807ed9d2a090aee0822cc
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-23 17:21:03 +0200

    my-company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Home'
 import About from './About'
 import Services from './Services'
@@ -13,10 +13,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
-  
-  </>
-  )
+  <Router>
+    <Routes>
+      <Route path ='/' element={<Home />} />
+      <Route path ='/' element={<About />} />
+      <Route path = '/' element={<Services />} />
+      <Route path ='/' element ={<Contact />} />
+    </Routes>
+  </Router>
+  );
 }
 
 export default App
