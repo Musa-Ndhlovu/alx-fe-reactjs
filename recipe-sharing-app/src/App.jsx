add594ecef1fce79ccf86228993588b3cec887d2
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-28 21:11:31 +0200

    recipe

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//zustand
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import recipeStore from './store/recipeStore' 
+
+
+//zustand
+import create from 'zustand'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+ 
+
+  return (
+    <>
+      <div>
+     
+      </div>
+      
+    </>
+  )
+}
+
+export default App
