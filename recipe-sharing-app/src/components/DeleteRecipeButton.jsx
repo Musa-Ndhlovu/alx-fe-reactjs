ca89828f97da8d150e3cf08910cf6b0196107204
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-02 16:33:02 +0200

    recipe

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,24 @@
-const button = ({text, onClick })  => {
+import {useState }from "react";
+import {useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
+
+const DeleteRecipeButton = ({ recipeId }) => {
+    const {deleteRecipe} = useRecipeStore();
+    const navigate = useNavigate();
+
+    const handleDelete = () => {
+        deleteRecipe(recipeId);
+        navigate("/")
+    }
+}
+
     return (
         <button 
-        onClick = {onClick}
+        onClick = {handleDelete} 
         className= "backgroundColor: green text: black hover:backgroundColor blue">
-
+        Delete Button
         </button>
     )
-}
+
 
 export default DeleteRecipeButton
