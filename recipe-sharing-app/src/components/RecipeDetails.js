050b26ef3e37a2b558c1bdc88d5c48923c59c3b9
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-02 15:28:59 +0200

    recipe

--- recipe-sharing-app/src/components/RecipeDetails.js
+++ recipe-sharing-app/src/components/RecipeDetails.js
@@ -1,16 +1,29 @@
  // RecipeDetails component
  import { useRecipeStore } from './recipeStore';
 
- const RecipeDetails = ({ recipeId }) => {
-   const recipe = useRecipeStore(state =>
-     state.recipes.find(recipe => recipe.id === recipeId)
-   );
+ const RecipeDetails = ({ recipeDetails}) =>
+     recipe = useRecipeStore (state => 
+        state.recipe?.find(recipe => recipe.id === recipeDetails.id)
+    );
 
-   return (
-     <div>
-       <h1>{recipe.title}</h1>
-       <p>{recipe.description}</p>
-       {/* Render EditRecipeForm and DeleteRecipeButton here */}
-     </div>
-   );
- };
+    if (!recipe) {
+        return <p>Recipe not found</p>
+    }
+
+    return (
+        <div>
+            <h1>{recipe.title}</h1>
+            <p>{recipe.description}</p>
+
+    <button type = "button" className="bg-green text-white rounded">
+        Edit Recipe 
+    </button>
+
+    <button type = "button" className = "bg-green text-white rounded">
+        Delete Recipe 
+    </button>
+    </div>
+    );
+
+
+export  default RecipeDetails;
