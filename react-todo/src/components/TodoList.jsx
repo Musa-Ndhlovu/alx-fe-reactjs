0863d4ed59c84245be777454a3dd4844251d4645
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-09 17:21:49 +0200

    Attempting Step 2

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react";
-import AddTodoForm from '..'
+import AddTodoForm from "./AddTodoForm";
 
 const Todolist =  () => {
-    const { TodoList, removeTodoList, toggleTodoList } = useTodoStore 
+    const { TodoList, removeTodoList, toggleTodoList } = useTodoStore();
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+        
+        <input
+        type = "checkbox"
+        checked = {TextTrackList.completed}
+        removed = {TextTrackList.deleted}
+        onchange = {() => toggleTodoList}
+        onChange = {() => removeTodoList}>
+        </input>
+
+        
+        <button 
+        onClick = {() => AddTodoForm }> AddTodo </button>
+        <button 
+        onClick = {() => removeTodoList}> RemoveTodo</button>
+        </div>
+    )
 }
 
 export default TodoList
