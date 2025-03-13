b0230444a1b7733be6891e172c6732a4a5668ca0
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-13 14:48:34 +0200

    task 2 part 4

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div class="md:p-8, sm:p-4">
+        <div class="md:p-8, sm:p-4, max-w-xs, md:max-w-sm ">
       <div bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg className="user-profile"> 
         <img rounded-full w-36 h-36 mx-auto  md:w-36h-36 sm:w-24 h-24  src="https://via.placeholder.com/150" alt="User" />
         <h1 text-xl text-blue-800 my-4 md:text-xl sm:text-lg >John Doe</h1>
