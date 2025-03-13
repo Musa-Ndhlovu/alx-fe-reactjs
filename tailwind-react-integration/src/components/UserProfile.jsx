9f6f30f25898a4b14659350d73b6421ae3376a4d
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-13 14:38:44 +0200

    styling task 2

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,11 @@
 function UserProfile() {
     return (
+        <div class="sm: p-4">
       <div bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg className="user-profile"> 
-        <img rounded-full w-36 h-36 mx-auto src="https://via.placeholder.com/150" alt="User" />
-        <h1 >John Doe</h1>
-        <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <img rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36h-36 src="https://via.placeholder.com/150" alt="User" />
+        <h1 text-xl text-blue-800 my-4  sm:text-lg md:text-xl>John Doe</h1>
+        <p text-gray-600 text-base sm:text-sm md:text-base >Developer at Example Co. Loves to write code and explore new technologies.</p>
+      </div>
       </div>
     );
   }
