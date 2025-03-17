58dc1d0677cc14e82b7132f4e594a0014a94dc9c
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-17 21:47:30 +0200

    AddRecipeForm

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -14,4 +14,52 @@ const AddRecipeForm = ({onAddRecipe}) => {
           }
 }
 }
+
+return 
+    <div className="max-w-xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+    <h2 className="text-2xl font-bold mb-4">Add a New Recipe</h2>
+    <form onSubmit={handleSubmit} className="space-y-4">
+   
+      <div>
+        <label className="block font-medium">Recipe</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full p-2 border rounded-md"
+          placeholder="Enter recipe title"
+          required
+        />
+      </div>
+
+      
+      <div>
+        <label className="block font-medium">Ingredients</label>
+        <textarea
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+          className="w-full p-2 border rounded-md"
+          placeholder="List ingredients, one per line"
+          rows="4"
+          required
+        ></textarea>
+      </div>
+
+     
+      <div>
+        <label className="block font-medium">Preparation </label>
+        <textarea
+          value={instructions}
+          onChange={(e) => setInstructions(e.target.value)}
+          className="w-full p-2 border rounded-md"
+          placeholder="List steps, one per line"
+          rows="4"
+          required
+        ></textarea>
+      </div>
+
+
 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+
+</form>
+</div>
