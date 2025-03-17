f9c74775afa4b88d1ea14417815d890ae825bbf0
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-17 21:17:20 +0200

    task 2 - Recipe Detail 2

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -19,7 +19,7 @@ const RecipeDetail= () => {
         <div className="mx-auto font-lg">
             <img src={recipe.image} alt={recipe.title} className="rounded-lg" />
             <h1 className="text-xl">Ingredients</h1>
-            <li key={index}>{ingredient}</li>
+            <li key={index}>{ingredients}</li>
         </div>
     )
 
