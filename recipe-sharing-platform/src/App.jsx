47aa782a35b1764eb346f3a1376f3b63b8216613
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-03-17 20:33:53 +0200

    react-router-dom

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -8,6 +8,10 @@ import React from 'react'
 import tailwindConfig from '../tailwind.config'
 import HomePage from './components/HomePage'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
 
 <HomePage />
   
@@ -17,6 +21,12 @@ function App() {
   return (
     <>
     
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
       
     </>
   )
